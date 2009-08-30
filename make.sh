c70497fcce16affe17bd80ec1479c854780a758d
David Wolinsky <davidiw@ufl.edu>
2009-08-30 17:34:28 -0400

    Initial commit, intro done

---
+++ make.sh
@@ -0,0 +1,7 @@
+latex nsdi10 -interaction=nonstopmode
+bibtex nsdi10
+latex nsdi10 -interaction=nonstopmode
+latex nsdi10 -interaction=nonstopmode
+dvips nsdi10.dvi
+ps2pdf nsdi10.ps
+xpdf nsdi10.pdf
