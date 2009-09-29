a622f1198d04c8fc564f19a850c525e46980c4da
David Wolinsky <davidiw@ufl.edu>
2009-09-29 14:27:07 -0400

    Finished Full Tunneling

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@ latex nsdi10 -interaction=nonstopmode
 bibtex nsdi10
 latex nsdi10 -interaction=nonstopmode
 latex nsdi10 -interaction=nonstopmode
-dvips nsdi10.dvi
+dvips -t letter nsdi10.dvi
 ps2pdf nsdi10.ps
 xpdf nsdi10.pdf
