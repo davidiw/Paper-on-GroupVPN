389b3a4093883922fed5fabb138fcaa0062b448e
David Wolinsky <davidiw@ufl.edu>
2010-11-03 18:15:32 -0400

    Accepted at CollaborateCom

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 latex GroupVPN -interaction=nonstopmode
-bibtex GroupVPN
-latex GroupVPN -interaction=nonstopmode
-latex GroupVPN -interaction=nonstopmode
+#bibtex GroupVPN
+#latex GroupVPN -interaction=nonstopmode
+#latex GroupVPN -interaction=nonstopmode
 dvips -t letter GroupVPN.dvi
 ps2pdf GroupVPN.ps
 xpdf GroupVPN.pdf
